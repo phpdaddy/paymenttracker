c03bd339092ed7d5cae293cc236f004e8e221374
Maksym Prysiazhnyi <m.prysiazhnyi@kendaxa.com>
2017-10-28 19:57:10 +0200

    Encapsulate payment to an entity

---
+++ src/main/java/Payment.java
@@ -0,0 +1,20 @@
+public class Payment {
+    private Currency currency;
+    private Integer value;
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
