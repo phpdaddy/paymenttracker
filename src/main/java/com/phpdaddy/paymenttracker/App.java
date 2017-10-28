e7979536fcf4b141bdbd7b613418f5494193536c
Maksym Prysiazhnyi <m.prysiazhnyi@kendaxa.com>
2017-10-28 21:11:58 +0200

    Add option to read payments from file

--- src/main/java/com/phpdaddy/paymenttracker/App.java
+++ src/main/java/com/phpdaddy/paymenttracker/App.java
@@ -4,6 +4,7 @@ import com.phpdaddy.paymenttracker.model.Currency;
 import com.phpdaddy.paymenttracker.model.Payment;
 import com.phpdaddy.paymenttracker.service.PaymentService;
 
+import java.io.*;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Timer;
@@ -14,6 +15,10 @@ public class App {
     private final static HashMap<Currency, Integer> balance = new HashMap<>();
 
     public static void main(String[] args) {
+        if (args.length == 1) {
+            File file = new File(args[0]);
+            readPaymentsFromFile(file);
+        }
         scheduleNetAmounts();
         while (true) {
             System.out.println("Enter payment in format 'USD 123' or 'quit' for exit or 'show' to show net amounts");
@@ -22,16 +27,39 @@ public class App {
     }
 
     private static void processInput() {
-        Payment payment;
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
+
+        if (line.equals("quit")) {
+            System.exit(0);
+        } else if (line.equals("show")) {
+            showNetAmounts();
+            return;
+        }
+        readPayments(line);
+    }
+
+    private static void readPaymentsFromFile(File file) {
+        FileReader fileReader;
         try {
-            if (line.equals("quit")) {
-                System.exit(0);
-            } else if (line.equals("show")) {
-                showNetAmounts();
-                return;
+            fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                readPayments(line);
             }
+            bufferedReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + file.getAbsolutePath() + "'");
+        } catch (IOException ex) {
+            System.out.println("Error reading file '" + file.getAbsolutePath() + "'");
+        }
+    }
+
+
+    private static void readPayments(String line) {
+        Payment payment;
+        try {
             payment = paymentService.readPayment(line);
         } catch (RuntimeException ex) {
             System.out.println("!!! " + ex.getLocalizedMessage());
@@ -41,7 +69,12 @@ public class App {
         if (balance.get(payment.getCurrency()) != null) {
             currentBalance = balance.get(payment.getCurrency());
         }
-        balance.put(payment.getCurrency(), currentBalance + payment.getValue());
+        int newBalance = currentBalance + payment.getValue();
+        if (newBalance <= 0) {
+            balance.remove(payment.getCurrency());
+            return;
+        }
+        balance.put(payment.getCurrency(), newBalance);
     }
 
     private static void scheduleNetAmounts() {
